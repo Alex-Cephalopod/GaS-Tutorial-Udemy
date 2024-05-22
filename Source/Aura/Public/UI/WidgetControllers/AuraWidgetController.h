23da4df2124a40b6e53a0f5e94f008ff410821d6
Alex-Cephalopod <76233802+Alex-Cephalopod@users.noreply.github.com>
2024-05-21 23:59:04 -0400

    gample bili tea sys

---
+++ Source/Aura/Public/UI/WidgetControllers/AuraWidgetController.h
@@ -0,0 +1,38 @@
+// Copyright Cephalo-Alex
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "AuraWidgetController.generated.h"
+
+/**
+ * 
+ */
+
+class UAbilitySystemComponent;
+class UAttributeSet;
+
+UCLASS()
+class AURA_API UAuraWidgetController : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "Widget Controller")
+		TObjectPtr<APlayerController> PlayerController;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Widget Controller")
+		TObjectPtr<APlayerState> PlayerState;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Widget Controller")
+		TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Widget Controller")
+		TObjectPtr<UAttributeSet> AttributeSet;
+
+private:
+	
+};
