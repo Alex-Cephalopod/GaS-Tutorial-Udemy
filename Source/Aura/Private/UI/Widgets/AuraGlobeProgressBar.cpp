177d7fad912313fa0afbe84c8d1eeee8239e5309
Alex-Cephalopod <76233802+Alex-Cephalopod@users.noreply.github.com>
2024-05-23 01:49:42 -0400

    turned blueprint logic to code :)

---
+++ Source/Aura/Private/UI/Widgets/AuraGlobeProgressBar.cpp
@@ -0,0 +1,79 @@
+// Copyright Cephalo-Alex
+
+
+#include "UI/Widgets/AuraGlobeProgressBar.h"
+#include "Components/SizeBox.h"
+#include "Components/ProgressBar.h"
+#include "Components/Image.h"
+#include "Components/OverlaySlot.h"
+#include "Blueprint/WidgetLayoutLibrary.h"
+//#include "Layout/Widget"
+//#include "Styling/SlateBrush.h"
+
+void UAuraGlobeProgressBar::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	UpdateBoxSize();
+	UpdateBackgroundBrush();
+	UpdateGlobeImage();
+	UpdateGlobePadding();
+	UpdateGlassBrush();
+	UpdateGlassPadding();
+}
+
+void UAuraGlobeProgressBar::UpdateBoxSize()
+{
+	SizeBox_Root->SetWidthOverride(BoxWidth);
+	SizeBox_Root->SetHeightOverride(BoxHeight);
+}
+
+void UAuraGlobeProgressBar::UpdateBackgroundBrush()
+{
+	Image_Background->SetBrush(BackgroundBrush);
+}
+
+void UAuraGlobeProgressBar::UpdateGlobeImage()
+{
+	FLinearColor TransparentBG;
+	TransparentBG.A = 0;
+
+	FSlateColor Transparent = FSlateColor(TransparentBG);
+
+	FSlateBrush TransparentBrush;
+	TransparentBrush.TintColor = Transparent;
+
+	FProgressBarStyle PBarStyle;
+	PBarStyle.BackgroundImage = TransparentBrush;
+	PBarStyle.FillImage = ProgressBarFillImage;
+
+	ProgressBar_Globe->WidgetStyle = PBarStyle;
+
+}
+
+void UAuraGlobeProgressBar::UpdateGlobePadding()
+{
+	UOverlaySlot* AsOverlay = UWidgetLayoutLibrary::SlotAsOverlaySlot(ProgressBar_Globe);
+
+	FMargin PaddingMargin = FMargin(GlobePadding, GlobePadding, GlobePadding, GlobePadding);
+
+	check(AsOverlay);
+
+	AsOverlay->SetPadding(PaddingMargin);
+}
+
+void UAuraGlobeProgressBar::UpdateGlassBrush()
+{
+	Image_Glass->SetBrush(GlassBrush);
+}
+
+void UAuraGlobeProgressBar::UpdateGlassPadding()
+{
+	UOverlaySlot* AsOverlay = UWidgetLayoutLibrary::SlotAsOverlaySlot(Image_Glass);
+
+	FMargin PaddingMargin = FMargin(GlobePadding, GlobePadding, GlobePadding, GlobePadding);
+
+	check(AsOverlay);
+
+	AsOverlay->SetPadding(PaddingMargin);
+}
---
+++ Source/Aura/Public/UI/Widgets/AuraGlobeProgressBar.h
@@ -0,0 +1,80 @@
+// Copyright Cephalo-Alex
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widgets/AuraUserWidget.h"
+//#include "Styling/SlateBrush.h"
+#include "AuraGlobeProgressBar.generated.h"
+
+/**
+ * 
+ */
+
+class USizeBox;
+class UImage;
+class UProgressBar;
+//struct FSlateBrush; 
+
+UCLASS()
+class AURA_API UAuraGlobeProgressBar : public UAuraUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void NativePreConstruct() override;
+
+	UFUNCTION()
+	void UpdateBoxSize();
+
+	UFUNCTION()
+	void UpdateBackgroundBrush();
+
+	UFUNCTION()
+	void UpdateGlobeImage();
+
+	UFUNCTION()
+	void UpdateGlobePadding();
+
+	UFUNCTION()
+	void UpdateGlassBrush();
+
+	UFUNCTION()
+	void UpdateGlassPadding();
+
+protected:
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+		USizeBox* SizeBox_Root;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+		UImage* Image_Background;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+		UProgressBar* ProgressBar_Globe;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+		UImage* Image_Glass;
+
+	UPROPERTY(EditAnywhere, Category = "Globe Properties")
+		float BoxWidth;
+
+	UPROPERTY(EditAnywhere, Category = "Globe Properties")
+		float BoxHeight;
+
+	UPROPERTY(EditAnywhere, Category = "Globe Properties")
+		FSlateBrush BackgroundBrush;
+
+	UPROPERTY(EditAnywhere, Category = "Globe Properties")
+		FSlateBrush ProgressBarFillImage;
+
+	UPROPERTY(EditAnywhere, Category = "Globe Properties")
+		float GlobePadding;
+
+	UPROPERTY(EditAnywhere, Category = "Globe Properties")
+		FSlateBrush GlassBrush;
+
+private:
+	
+};
